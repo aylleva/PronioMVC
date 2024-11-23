6ed7f9f1c0659e8be2df3f0062bba01fd968faeb
ayleva <ayllayeva@gmail.com>
2024-11-24 01:35:25 +0400

    single product update

--- ProniaMVC/Controllers/HomeController.cs
+++ ProniaMVC/Controllers/HomeController.cs
@@ -17,48 +17,15 @@ namespace ProniaMVC.Controllers
         public IActionResult Index()
         {
             
-            List<Slide> slides = new List<Slide>
-            {
-                new Slide
-                {Title="Flower 1",
-                SubTitle="Green color",
-                Description="Rare",
-                Image="1-1-524x617.png",
-                Order=2,
-                IsDeleted=false,
-                DateTime=DateTime.Now
+                HomeVM vm = new HomeVM {
+                Slides = _context.Slides
+                .OrderBy(s=>s.Order)
+                .Take(2)
+                .ToList(),
 
-                },
-
-                 new Slide
-                {Title="Flower 2",
-                SubTitle="Red color",
-                Description="Rare",
-                Image="images.jpeg",
-                Order=1,
-                IsDeleted=false,
-                DateTime=DateTime.Now
-
-                },
-                  new Slide
-                {Title="Flower 3",
-                SubTitle="Green color",
-                Description="Rare",
-                Image="1-2-524x617.png",
-                Order=3,
-                IsDeleted=false,
-                DateTime=DateTime.Now
-
-                }
-            }
-               ;
-
-            _context.Slides.AddRange(slides);  
-            _context.SaveChanges();
-
-            HomeVM vm = new HomeVM {
-                Slides = _context.Slides.OrderBy(s=>s.Order).Take(2).ToList(),
-                Products=_context.Products.Include(img=>img.Images).ToList(), 
+                Products=_context.Products
+                .Take(8)
+                .Include(img=>img.ProductImages.Where(pi=>pi.IsPrimary!=null)).ToList(), 
             };
             return View(vm);  
         }
--- ProniaMVC/Controllers/ShopController.cs
+++ ProniaMVC/Controllers/ShopController.cs
@@ -1,13 +1,46 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProniaMVC.DAL;
+using ProniaMVC.Models;
+using ProniaMVC.ViewModels;
 
 namespace ProniaMVC.Controllers
 {
     public class ShopController:Controller
 
     {
+        private readonly AppDBContext _context;
+
+        public ShopController(AppDBContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Detail(int? id)
+        {
+            if (id == null || id < 1) return BadRequest();
+
+            Product? product = _context.Products
+                .Include(p=>p.ProductImages.OrderByDescending(p=>p.IsPrimary))
+                .Include(p=>p.Category)
+                .FirstOrDefault(p => p.Id == id);
+
+            if (product is null) return NotFound();
+
+            DetailVM detailVM = new DetailVM
+            {
+                Product = product,
+                RelatedProducts = _context.Products
+                .Where(p => p.CategoryId == product.CategoryId && p.Id != id)
+                .Include(p=>p.ProductImages.Where(pi=>pi.IsPrimary!=null)) 
+                .ToList()
+            };
+
+            return View(detailVM);
         }
     }
 }
